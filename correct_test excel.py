d8ea0f8fc101143a3bf22a890fd2667d000e94ec
Lieshan6148 <1908977674@qq.com>
2025-03-13 15:36:59 +0800

    --

--- correct_test excel.py
+++ correct_test excel.py
@@ -13,7 +13,7 @@ latticeType=1
 dimension=100
 q=128
 blksize=2
-for dimension in range(1, 121):  # 假设我们写入10行数据
+for dimension in range(1, 100):  # 假设我们写入10行数据
     print("dimension:",dimension)
     # if (selectmode(latticeType,dimension,blksize,q)):
     #     _ = ws.cell(row=dimension, column=1, value=blksize)
@@ -29,6 +29,7 @@ for dimension in range(1, 121):  # 假设我们写入10行数据
         _ = ws.cell(row=dimension+1, column=1, value=dimension)
         _ = ws.cell(row=dimension+1, column=2, value=blksize)
         _ = ws.cell(row=dimension+1, column=3, value=times)
+        wb.save("最小blk.xlsx")
 
 # 保存工作簿到文件
 wb.save("最小blk.xlsx")
